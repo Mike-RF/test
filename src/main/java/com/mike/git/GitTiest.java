ed9ca303f12c2fb894f4dae35ab23ab8091c5631
MikeRF <577965445@qq.com>
2021-06-19 22:41:02 +0800

    master 2

--- src/main/java/com/mike/git/GitTiest.java
+++ src/main/java/com/mike/git/GitTiest.java
@@ -4,5 +4,6 @@ public class GitTiest {
     public static void main(String[] args) {
         System.out.println("master111");
         System.out.println("hot_fix111");
+        System.out.println("master222");
     }
 }
