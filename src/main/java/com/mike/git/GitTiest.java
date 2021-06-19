6d635d42a9fe870be30b02272fe90ec6e4f2051d
MikeRF <577965445@qq.com>
2021-06-19 22:36:25 +0800

    master 1

--- src/main/java/com/mike/git/GitTiest.java
+++ src/main/java/com/mike/git/GitTiest.java
@@ -2,6 +2,6 @@ package com.mike.git;
 
 public class GitTiest {
     public static void main(String[] args) {
-        System.out.println("hello world!!!");
+        System.out.println("master111");
     }
 }
